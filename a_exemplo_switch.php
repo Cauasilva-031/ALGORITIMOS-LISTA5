2ef2f0f4bc8a442beb20bf31620fb9a5f860c9fc
Cauã Silva <deepplay50@gmail.com>
2026-07-22 19:19:45 -0300

    exemplo switch

---
+++ a_exemplo_switch.php
@@ -0,0 +1,51 @@
+<?php
+$dia = 3;
+$valor = 100.00;
+
+switch($dia){
+    case 1:
+        echo("domingo");
+        break;
+        case 2:
+            echo("segunda");
+            break;
+            case 3:
+                echo("terça-feira");
+                break;
+                case 4:
+                    echo("quarta-feira");
+                    break;
+                    case 5:
+                        echo("quinta-feira");
+                        break;
+                        case 6:
+                            echo("sexta-feira");
+                            break;
+                            default:
+                            echo("opção invalida");
+                            break;
+
+                            
+                        
+
+}
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
